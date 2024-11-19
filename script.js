0873c7d2d2679d65d51e3dd52c0abfb57f6fe91f
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-19 17:32:53 +0100

    fine js

--- script.js
+++ script.js
@@ -9,16 +9,28 @@ const params = { _limit: 6 };
 
 axios.get(baseUrl + resource, { params }).then((res) => {
     const images = res.data;
-    
+
+    const container = document.querySelector('.container');
+
     for (let i = 0; i < images.length; i++) {
-        const image = images[i];  
-        const title = image.title;  
+        const image = images[i];
+        const title = image.title;
         const thumbnailUrl = image.thumbnailUrl;
 
-        // Logga i valori nel console
         console.log(`Immagine ${i + 1}:`);
         console.log(`Title: ${title}`);
         console.log(`Thumbnail URL: ${thumbnailUrl}`);
+
+        container.innerHTML += `
+        <div class="card">
+            <div class = "img">
+                <img src="${thumbnailUrl}" alt="${title}">         
+            </div>
+            <div class="text">
+                <p>${title}</p>
+            <div/>
+        </div>
+        `;
     }
 }).catch((error) => {
     console.error('Errore nella richiesta:', error);
